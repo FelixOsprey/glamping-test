2d81d5273da54bdc4a204a98f98d87185614024a
FelixOsprey <mauziogt@gmail.com>
2025-11-13 11:10:58 +0100

    Base site setup, react router setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRoutes } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import Stays from "./pages/Stays";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
+// Parent komponent som styrer visninger af andre komponenter
 function App() {
-  const [count, setCount] = useState(0)
+  // Vha react-router definerer vi hvilke komponenter der hører til hvilke paths
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/stays", element: <Stays /> },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Navigation />
+      <main>{routes}</main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+/* Browserrouter skal være 'container' for App (altså hele projektet) 
+Den fungerer som en såkaldt 'provider' der sørger for at renderer 
+forskelligt indhold (komponenter) baseret på url'en.
+*/
+
+// Her hentes html-elementet med id'et 'root' fra dommen. Dommen 'kobles' på.
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
